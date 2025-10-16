5b452d5bd7e7f200b4022832854dd080f46a1b96
Vitor Fagundes <vitorfagundes.7@gmail.com>
2025-10-15 21:52:15 -0300

    adiconando fxtures

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http' //Importação de módulo
 import { sleep, check } from 'k6' //Importação de função
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = {
   stages: [
@@ -7,18 +8,17 @@ export const options = {
     { duration: '20s', target: 10 },
     { duration: '5s', target: 0 }
   ],
+  //iterations: 1,
   thresholds: {
-    http_req_failed: ['rate<0.01'],
+    http_req_failed: ['rate<50'],
     http_req_duration: ['p(90)<3000', 'max<5000'],
   }
 }
 
 export default function () {
   const url = 'http://localhost:3000/login';
-  const payload = JSON.stringify({
-    username: 'julio.lima',
-    senha: '123456',
-  });
+  console.log(postLogin);
+  const payload = JSON.stringify({postLogin});
 
   const params = {
     headers: {
