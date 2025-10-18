e6d8912214099475de7a80058359a66967c3e301
Vitor Fagundes <vitorfagundes.7@gmail.com>
2025-10-18 15:30:29 -0300

    adicionando a função helper para  obtenção de token

--- tests/login.test.js
+++ tests/login.test.js
@@ -17,8 +17,9 @@ export const options = {
 
 export default function () {
   const url = 'http://localhost:3000/login';
+  
   console.log(postLogin);
-  const payload = JSON.stringify({postLogin});
+  const payload = JSON.stringify({ postLogin });
 
   const params = {
     headers: {
---
+++ tests/transferencias.test.js
@@ -0,0 +1,37 @@
+import http from 'k6/http' //Importação de módulo
+import { sleep, check } from 'k6' //Importação de função
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
+import { obterToken } from '../helpers/autenticacao.js';
+
+export const options = {
+  iterations: 1,
+  duration: '30s',
+};
+
+export default function () {
+  const token = obterToken()
+
+  const url = 'http://localhost:3000/transferencias';
+
+  const payload = JSON.stringify({
+    contaOrigem: 1,
+    contaDestino: 2,
+    value: 11,
+    token: ""
+  })
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token
+    },
+  }
+
+  const res = http.post(url, payload, params);
+
+  check(res, {
+    'status is 201': (res) => res.status == 201
+  })
+
+  sleep(1)
+}
