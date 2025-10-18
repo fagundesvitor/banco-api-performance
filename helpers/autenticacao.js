93cf1aacc280f94b737a659fb58dd984400a5410
Vitor Fagundes <vitorfagundes.7@gmail.com>
2025-10-18 16:00:03 -0300

    criação da variavel local

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,10 +1,11 @@
 import http from 'k6/http' //Importação de módulo
 import { sleep, check } from 'k6' //Importação de função
+import { pegarBaseURL } from '../utils/variaveis.js';
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 
 export function obterToken() {
-  const url = 'http://localhost:3000/login';
+  const url = pegarBaseURL() + '/login';
 
   const payload = JSON.stringify({ postLogin });
 
--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http' //Importação de módulo
 import { sleep, check } from 'k6' //Importação de função
+import { pegarBaseURL } from '../utils/variaveis.js';
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = {
@@ -16,8 +17,8 @@ export const options = {
 }
 
 export default function () {
-  const url = 'http://localhost:3000/login';
-  
+  const url = pegarBaseURL() + '/login';
+
   console.log(postLogin);
   const payload = JSON.stringify({ postLogin });
 
